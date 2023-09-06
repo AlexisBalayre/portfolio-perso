83de23942c0ac5e960b126effb4ab2b8622a1342
AlexisBal <alexis.balayre@eleve.isep.fr>
2023-09-06 23:02:20 +0200

    New version
    Signed-off-by: AlexisBal <alexis.balayre@eleve.isep.fr>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,26 +2,26 @@ export default defineNuxtConfig({
     app: {
         pageTransition: { name: 'page', mode: 'out-in' },
         head: {
-            title: 'Alexis Balayre - Full Stack Web3 Developer',
+            title: 'Alexis Balayre - Full Stack Web and Blockchain Developer | Data Engineer',
             htmlAttrs: {
               lang: 'en'
             },
             meta: [
               { charset: 'utf-8' },
               { name: 'viewport', content: 'width=device-width, initial-scale=1 viewport-fit=cover' },
-              { hid: 'description', name: 'description', content: 'Full stack developer specialized in Web3 (Blockchain, Smart Contracts, DApp)', id:"__meta-description"  },
+              { hid: 'description', name: 'description', content: 'Data engineer and full stack Web and Blockchain developer specialized in Web3 (Smart Contracts, DApp).', id:"__meta-description"  },
               { name: 'format-detection', content: 'telephone=no' },
               { hid: 'og:type', name: 'og:type', content: 'website' },
-              { hid: 'og:site_name', name: 'og:site_name', content: 'Alexis Balayre - Full Stack Web3 Developer' },
-              { hid: 'og:title', name: 'og:title', content: 'Alexis Balayre - Full Stack Web3 Developer' },
-              { hid: 'og:description', name: 'og:description', content: 'Full stack developer specialized in Web3 (Blockchain, Smart Contracts, DApp)', id:"__meta-og:description" },
+              { hid: 'og:site_name', name: 'og:site_name', content: 'Alexis Balayre - Full Stack Web and Blockchain Developer | Data Engineer' },
+              { hid: 'og:title', name: 'og:title', content: 'Alexis Balayre - Full Stack Web and Blockchain Developer | Data Engineer' },
+              { hid: 'og:description', name: 'og:description', content: 'Data engineer and full stack Web and Blockchain developer specialized in Web3 (Smart Contracts, DApp).', id:"__meta-og:description" },
               { hid: 'og:image', name: 'og:image', content: 'https://alexis.balayre.com/preview.png' },
               { hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image' },
               { hid: 'twitter:site', name: 'twitter:site', content: '@Belas_Eth' },
               { hid: 'twitter:title', name: 'twitter:title', content: 'Alexis Balayre' },
-              { hid: 'twitter:description', name: 'twitter:description', content: 'Full stack developer specialized in Web3 (Blockchain, Smart Contracts, DApp)', id:"__meta-twitter:description" },
+              { hid: 'twitter:description', name: 'twitter:description', content: 'Data engineer and full stack Web and Blockchain developer specialized in Web3 (Smart Contracts, DApp).', id:"__meta-twitter:description" },
               { hid: 'twitter:image', name: 'twitter:image', content: 'https://alexis.balayre.com/preview.png' },
-              { hid: 'keywords', name: 'keywords', content: 'Alexis Balayre, Blockchain, Solidity, Web3, Developer, Full Stack, Ethereum, DApp, Smart Contracts' },
+              { hid: 'keywords', name: 'keywords', content: 'Alexis Balayre, Blockchain, Solidity, Web3, Developer, Full Stack, Ethereum, DApp, Smart Contracts, Data, Data Engineer' },
 
             ],
             link: [
